0c24b0abf3c32e828344a2b7c3d7c5e857028849
David Claveau <daveclaveau@gmail.com>
2021-05-21 02:26:11 +0000

    Form toggle focuses compose tweet; WIP for jump-to-top button

--- public/scripts/form-toggle.js
+++ public/scripts/form-toggle.js
@@ -3,8 +3,10 @@ $(document).ready(() => {
   // Clicking "Write a new tweet"
   $(".write-tweet-nav").on("click", () => {
     const $newTweet = $(".new-tweet");
+    const $tweetText = $("#tweet-text");
     if ($newTweet.css("display") === "none") {
       $newTweet.slideDown();
+      $tweetText.focus();
     } else {
       $newTweet.slideUp();
     }
